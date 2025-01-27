94a0fbb21ff89e472ca4dd106a2030198be33322
jmngandu <jamesmngandu@gmail.com>
2025-01-27 18:05:40 +0300

    commit

--- REACT/react-one/src/App.jsx
+++ REACT/react-one/src/App.jsx
@@ -1,9 +1,11 @@
+import Button from "./Button/Button";
 import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <div>
+        <Button />
         <Footer />
       </div>
     </>
---
+++ REACT/react-one/src/Button/Button.jsx
@@ -0,0 +1,5 @@
+import styles from "./Button.module.css";
+function Button() {
+  return <button className={styles.button}>Click Me!</button>;
+}
+export default Button;
