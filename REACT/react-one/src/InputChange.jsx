4808a3e06bed6009c86f7aea6f860f1c65573997
jmngandu <jamesmngandu@gmail.com>
2025-01-28 23:37:38 +0300

    fire-one

--- REACT/react-one/src/InputChange.jsx
+++ REACT/react-one/src/InputChange.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 
 export default function InputChange() {
   const [name, setName] = useState("Guest");
+  const [comment, setComment] = useState("");
   const handleChange = (e) => {
     setName(e.target.value);
+  };
+  const handleComment = (e) => {
+    setComment(e.target.value);
   };
   return (
     <>
       <input onChange={handleChange} value={name} />
       <p>Name: {name}</p>
+      <textarea
+        value={comment}
+        placeholder="Enter more details"
+        onChange={handleComment}
+      />
+      <p>Comment: {comment}</p>
     </>
   );
 }
