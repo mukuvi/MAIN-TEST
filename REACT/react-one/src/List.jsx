25be07c6b45748e0c7b302cfde1ebdc02927519e
jmngandu <jamesmngandu@gmail.com>
2025-01-28 20:14:48 +0300

    commit

--- REACT/react-one/src/List.jsx
+++ REACT/react-one/src/List.jsx
@@ -1,7 +1,7 @@
 function List() {
   const fruits = ["mango", "banana", "apple", "orange", "pineapple"];
   fruits.sort();
-  const fruitEl = fruits.map((fruit) => <li key={fruits}>{fruit}</li>);
+  const fruitEl = fruits.map((fruit) => <li key={fruit}>{fruit}</li>);
   return <ul>{fruitEl}</ul>;
 }
 export default List;
