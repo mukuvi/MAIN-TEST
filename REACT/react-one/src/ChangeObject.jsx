3a0dc11f7bfdaec93699d5166ca06eb37bfa2461
jmngandu <jamesmngandu@gmail.com>
2025-01-29 08:45:43 +0300

    commit

--- REACT/react-one/src/ChangeObject.jsx
+++ REACT/react-one/src/ChangeObject.jsx
@@ -6,11 +6,18 @@ export default function ChangeObject() {
     make: "toyota",
     model: "hilux",
   });
+  const handleYear = (e) => {
+    setCar({ year: 2025 });
+  };
   return (
     <>
       <p>
         i love {car.make} {car.model} {car.year}
       </p>
+      <input value={car.year} onChange={handleYear} />
+      <input value={car.make} />
+      <input value={car.model} />
+      <br />
     </>
   );
 }
