6a9cd167ae1e69afc0acfe6c1ea84126d042c511
jmngandu <jamesmngandu@gmail.com>
2025-01-30 10:52:07 +0300

    commit

--- REACT/react-one/src/App.jsx
+++ REACT/react-one/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         {/* <Counter /> */}
         {/* <List /> */}
         {/* <ToDoList /> */}
-        <EffectHook />
+
         <Button />
         <Footer />
       </div>
