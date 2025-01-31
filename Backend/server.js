b65cef3eb0f5035d4186a772b05a5a2194639270
jmngandu <jamesmngandu@gmail.com>
2025-01-31 12:03:50 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+app.get("/", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
 });
