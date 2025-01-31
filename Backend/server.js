c8475d3f4a5102d4371f9728b90772602921bd21
jmngandu <jamesmngandu@gmail.com>
2025-01-31 13:09:07 +0300

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8383;
-const data = {
-  name: "james",
-};
+let data = ["james"];
+
+//middleware
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send(`<body><h1>DATA</h1><p>${JSON.stringify(data)}</p> </body>`);
 });
 // CRUD-method      Create-post    Read-get Update-put      Delete-delete
+
 app.get("/api/data", (req, res) => {
   res.send(data);
 });
-app.listen(PORT, () => {
-  console.log(`server has started on port: ${PORT}`);
+
+app.post("/api/data", (req, res) => {
+  const newEntry = req.body;
+  console.log(newEntry);
+  data.push(newEntry.name);
+  res.sendStatus(201);
 });
+
+app.listen(PORT, () => console.log(`server has started on port: ${PORT}`));
