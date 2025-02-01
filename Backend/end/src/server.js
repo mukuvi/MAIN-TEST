860acb139e1af897aa6664482dda50dbc43170df
jmngandu <jamesmngandu@gmail.com>
2025-02-01 15:40:48 +0300

    commit

--- Backend/end/src/server.js
+++ Backend/end/src/server.js
@@ -6,9 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 //Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url);
+//Get the directory name from the file path
+const __dirname = dirname(__filename);
 
+//Serves the HTML file from the /public directory
+//Tells express to serve all the files from the public folder as static assets /file.  Any request for CSS files will be resolved to the public directory
+app.use(express.static(path.join(__dirname, "../public")));
+
+//Serving up the HTML file from  the /public directory
 app.get("/", (req, res) => {
-  res.sendFile(path);
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
