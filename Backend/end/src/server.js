fa5127e7ff17b51d9685e48bbd56e5c1124fd9d3
jmngandu <jamesmngandu@gmail.com>
2025-02-01 15:28:00 +0300

    commit

--- Backend/end/src/server.js
+++ Backend/end/src/server.js
@@ -4,7 +4,8 @@ import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+//Get the file path from the URL of the current module
+const __filename = fileURLToPath();
 app.get("/", (req, res) => {
   res.sendFile(path);
 });
