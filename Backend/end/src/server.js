bd8d7a29d41449f3bd0b803f328e16062694d21d
jmngandu <jamesmngandu@gmail.com>
2025-02-01 15:23:15 +0300

    commit

--- Backend/end/src/server.js
+++ Backend/end/src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path);
+});
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
