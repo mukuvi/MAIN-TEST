cee4d0f51ca89d83cd60c3af00764acf5bbf7643
jmngandu <jamesmngandu@gmail.com>
2025-02-01 16:00:15 +0300

    commit

--- Backend/end/src/db.js
+++ Backend/end/src/db.js
@@ -2,7 +2,7 @@ import { DatabaseSync } from "node:sqlite";
 const db = new DatabaseSync(":memory:");
 //Execute SQL statements from strings
 db.exec(`
-    CREATE TABLE user (
+    CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
     password TEXT)`);
