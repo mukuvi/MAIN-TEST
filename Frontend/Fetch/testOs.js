4b6e4d2e19c469139823f13cd5a2a6052c985fbf
mukuvi <jamesmngandu@gmail.com>
2025-02-25 21:12:17 +0300

    commit

--- Frontend/Fetch/testOs.js
+++ Frontend/Fetch/testOs.js
@@ -1,6 +1,15 @@
 async function testOs() {
+  const userId = document.getElementById("userId").value;
+  const albumName = document.getElementById("userName");
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/albums/${userId}`
+    );
+    if (!response.ok) {
+      throw new Error("Response is not ok");
+    }
+    const album = await response.json();
+    albumName.innerHTML = `${album.title}`;
   } catch (error) {
     console.log(`${error}`);
   }
