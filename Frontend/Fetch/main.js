c63aa06d0da36d954f54511a2b3b45aea347b782
mukuvi <jamesmngandu@gmail.com>
2025-02-25 12:47:47 +0300

    commit

--- Frontend/Fetch/main.js
+++ Frontend/Fetch/main.js
@@ -1,6 +1,6 @@
 async function makeData() {
-  const message = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = response.json();
+  console.log(data);
 }
 makeData();
