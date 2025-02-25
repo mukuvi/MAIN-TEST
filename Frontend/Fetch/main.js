45b29dc05279e4c3abc8d888a816c868d31fac53
mukuvi <jamesmngandu@gmail.com>
2025-02-25 13:53:52 +0300

    commit

--- Frontend/Fetch/main.js
+++ Frontend/Fetch/main.js
@@ -7,3 +7,23 @@ const message = fetch("https://jsonplaceholder.typicode.com/users/1")
   })
   .then((data) => console.log(data))
   .catch((err) => console.log("Wrong syntax"));
+
+//   async function myData() {
+//     const dataId = document.getElementById("dataId").value;
+//     const dataObject = document.getElementById("dataObject");
+
+//     try {
+//       const response = await fetch(
+//         `https://jsonplaceholder.typicode.com/users/${dataId}`
+//       );
+//       if (!response.ok) {
+//         throw new Error("Response is not ok!");
+//       }
+//       const data = await response.json();
+//       const dataTitle = data.name;
+//       dataObject.textContent = dataTitle;
+//     } catch (error) {
+//       console.log(`${error}`);
+//       dataObject.textContent = "Error fetching data.";
+//     }
+//   }
