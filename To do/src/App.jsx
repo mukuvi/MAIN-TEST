5e1e303e35b50e12d1338c1183e24fc5060ac328
mukuvi <jamesmngandu@gmail.com>
2025-04-07 11:42:03 +0300

    maintest

--- To do/src/App.jsx
+++ To do/src/App.jsx
@@ -1,17 +1,4 @@
-import Button from "./Button";
-import ChangeObject from "./ChangeObject";
-import ChangeUpdate from "./ChangeUpdate";
-import Counter from "./Counter";
-
-import Footer from "./Footer";
-
-import Info from "./Info";
-import InputChange from "./InputChange";
-import List from "./List";
 import ToDoList from "./ToDoList";
-
-import UpdateArrays from "./UpdateArrays";
-import UpdatingObjects from "./UpdatingObjects";
 
 import "./index.css";
 
@@ -19,33 +6,7 @@ function App() {
   return (
     <>
       <div>
-        {/* <div className="info-data">
-          <Info
-            name="james"
-            age="21"
-            school="school of science"
-            isGender={true}
-          />
-          <Info
-            name="julia"
-            age="18"
-            school="school of medicine"
-            isGender={false}
-          />
-        </div> */}
-
-        {/* <ChangeUpdate />
-        <UpdateArrays /> */}
-        {/* <UpdatingObjects /> */}
-
-        {/* <InputChange /> */}
-        {/* <ChangeObject /> */}
-        {/* <Counter /> */}
-        {/* <List /> */}
         <ToDoList />
-
-        <Button />
-        <Footer />
       </div>
     </>
   );
